54c87c32d701e4c9bd73493939508aa92dcdca9b
xlucifer711 <106505627+xlucifer711@users.noreply.github.com>
2022-09-22 05:27:33 +0200

    Update __init__.py

--- jepthon/__init__.py
+++ jepthon/__init__.py
@@ -74,7 +74,7 @@ async def startup_process():
 
 async def externalrepo():
     if Config.VCMODE:
-        await install_externalrepo("https://github.com/xlucifer711/jepthon")
+        await install_externalrepo("https://github.com/jepthoniq/JepVc", "jepvc", "jepthonvc"")
 
 jepiq.loop.run_until_complete(externalrepo())
 jepiq.loop.run_until_complete(startup_process())
